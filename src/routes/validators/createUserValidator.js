7cec335a77641d4ede84edec0eed30c63d22dba2
davidhalasz <davidhalasz22@gmail.com>
2023-03-02 17:06:01 +0100

    remove telephone validator

--- src/routes/validators/createUserValidator.js
+++ src/routes/validators/createUserValidator.js
@@ -4,5 +4,5 @@ module.exports = [
     body('name', 'A felhasznalonevnek legalabb 4 karakterbol kell allnia.').isLength({min: 4}).trim().escape(),
     body('email', 'Az email nem valid').isEmail().trim().escape().normalizeEmail(),
     body('password', 'A jelszonak legalabb 6 karakterbol kell allnia.').isLength({min: 6}),
-    body('telephone', 'A telefonszámodnak 0 vagy legalább 10 karakterből kell állnia!').optional({checkFalsy: true ,nullable: true}).isLength({min: 10, max: 10}),
+    body('telephone', 'Túl sok karakter!').optional({checkFalsy: true ,nullable: true}).isLength({max: 40}),
 ]
